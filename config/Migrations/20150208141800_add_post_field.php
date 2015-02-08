b458dc85a9b10694c6c7c0e4f46efc65fac5c26e
James Tancock <tanuck@gmail.com>
2015-02-08 14:30:18 +0000

    Allow different post types

---
+++ config/Migrations/20150208141800_add_post_field.php
@@ -0,0 +1,42 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class AddPostField extends AbstractMigration {
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('posts');
+        $table->addColumn('type', 'string', [
+            'limit' => '5',
+            'after' => 'body'
+        ])
+        ->save();
+    }
+
+    /**
+     * Migrate Up.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * Migrate Down.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+}
--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Event\Event;
+use Cake\Network\Exception\NotFoundException;
 
 /**
  * Posts Controller
