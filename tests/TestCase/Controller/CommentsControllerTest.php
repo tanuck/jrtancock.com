ccbea53ae6203ec2c386ba2073e7866224cd219a
James Tancock <tanuck@gmail.com>
2015-02-13 14:45:35 +0000

    More tests & require-dev for PHPCS

--- tests/TestCase/Controller/CommentsControllerTest.php
+++ tests/TestCase/Controller/CommentsControllerTest.php
@@ -63,7 +63,40 @@ class CommentsControllerTest extends IntegrationTestCase
      */
     public function testEdit()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/comments/edit/1');
+        $this->assertRedirect(['controller' => 'users', 'action' => 'login']);
+
+        $this->session([
+            'Auth' => [
+                'User' => [
+                    'id' => 1,
+                    'username' => 'lorem'
+                ]
+            ]
+        ]);
+
+        $this->get('/comments/edit/1');
+        $this->assertResponseOk();
+
+        $this->get('/comments/edit');
+        $this->assertResponseCode(404);
+
+        $this->get('/comments/edit/100');
+        $this->assertResponseCode(404);
+
+        $data = [
+            'id' => 1,
+            'body' => 'edited blah blah'
+        ];
+        $this->post('/comments/edit/1', $data);
+        $this->assertRedirect(['action' => 'index']);
+        $comments = TableRegistry::get('Comments');
+        $comment = $comments->find()->where(['id' => 1])->first();
+        $this->assertEquals($data['body'], $comment->body);
+
+        $data['body'] = '';
+        $this->post('/comments/edit/1', $data);
+        $this->assertResponseContains('The comment could not be saved.');
     }
 
     /**
