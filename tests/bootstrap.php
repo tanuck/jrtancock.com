bc1f9ab377c170803aafab7983c5d2524049d8c3
James Tancock <tanuck@gmail.com>
2015-02-26 21:35:29 +0000

    Fix for failing tests due to debug off.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,8 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+use Cake\Core\Configure;
+
 require dirname(__DIR__) . '/config/bootstrap.php';
+
+Configure::write('debug', true);
